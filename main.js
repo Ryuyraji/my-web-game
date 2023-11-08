28d42139a971a354cc2d5b8930c320db6d1e8531
Ryuyraji <134043852+Ryuyraji@users.noreply.github.com>
2023-11-09 01:24:10 +0900

    my game

--- main.js
+++ main.js
@@ -13,7 +13,11 @@ var dino = {
         ctx.fillStyle = 'green';
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-};
+}
+
+var img1 = new Image();
+img1.src = 'obstacle.png';
+
 
 class Cactus {
     constructor() {
@@ -25,29 +29,66 @@ class Cactus {
 
     draw() {
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+       // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(img1. this.x , this.y)
     }
 }
 
 var cactusObstacles = [];
 var timer = 0;
+var jumptimer = 0;
+var animation;
 
 function update() {
-    requestAnimationFrame(update);
+    animation = requestAnimationFrame(update);
     timer++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (timer % 120 === 0) {
+    if (timer % 200  === 0) {
         var cactus = new Cactus();
         cactusObstacles.push(cactus);
     }
 
-    cactusObstacles.forEach((a) => {
+    cactusObstacles.forEach((a,i,o) => {
+        if(a.x < 0){
+           o.splice(i,1)
+        }
         a.x--;
+        attack(dino,a); //장애물 요소마다 다 체크해야하니깐!
         a.draw();
     });
+    if(jump==true){
+        dino.y--;
+        jumptimer++;
+    }
+    if(jump==false){
+        if(dino.y < 200){
+            dino.y++;
+        }    
+    }
+    if(jumptimer > 100){
+        jump=false;
+        jumptimer = 0;
+    
+    }
 
     dino.draw();
 }
 
 update();
+
+//충돌 체크
+function attack(dino,cactus){
+ var xx = cactus.x - (dino.x + dino.width);
+ var yy = cactus.y- (dino.y + dino.height);
+ if(xx < 0 && yy < 0){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    cancelAnimationFrame(animation)
+ }
+}
+var jump=false;
+document.addEventListener('keydown',function(e){
+    if (e.code==='Space'){
+        jump=true;
+    }
+});
